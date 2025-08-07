45eabc5aacfcc902381ccf7f26f642b982e89d78
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-08-07 22:36:16 +0200

    First complete JSON parser (kinda)

--- Json.lean
+++ Json.lean
@@ -1,5 +1,5 @@
-
-
+import Lean
+open Lean (AssocList)
 
 namespace v0
 /-
@@ -655,10 +655,6 @@ def parseEverything (parser : String → Option (Json × String)) (input : Strin
 end v7
 
 namespace v8
--- do we go for objects or strings now? In any case, that's similar respectively
--- to how we have handled arrays and digits respectively.
--- NOTA: we NEED strings to manage objects (obviously ...), so that's string
--- first. Actually, this is gonna be easy AFAICT. Great!
 
 
 inductive Json
@@ -870,3 +866,262 @@ def parseEverything (parser : String → Option (Json × String)) (input : Strin
 --     v8.Json.string "Hello!"],
 --  "")
 end v8
+
+
+namespace v9
+-- Objects!
+
+-- TODO: the list for object is not really nice, but HashMap
+--       won't work for positivity reasons (???) and AssocList
+--       is from Lean, not std, has no repr, etc.
+--       Let's use the list first and later come with my own
+--       "Object" custom type (?) for a better API.
+
+
+inductive Json
+| null : Json
+| bool (b : Bool) : Json
+| number (n : Float) : Json
+| string (s : String) : Json
+| array (elements : List Json) : Json
+| object (members : List (String × Json)) : Json
+deriving Repr
+
+def parseNull (input : String): Option (Json × String) := do
+  guard (input.startsWith "null")
+  return (Json.null, input.drop 4)
+
+def parseBool (input : String): Option (Json × String) :=
+  if input.startsWith "true" then
+    some (Json.bool true, input.drop 4)
+  else if input.startsWith "false" then
+    some (Json.bool false, input.drop 5)
+  else
+    none
+
+def parseSign (input : String) : Option (Float × String) :=
+  if input.startsWith "-" then
+    some (-1.0, input.drop 1)
+  else
+    some (1.0, input)
+
+def parseDigit (input : String) : Option (Nat × String) := do
+  let char <- input.get? 0
+  guard char.isDigit
+  let digit := char.toNat - '0'.toNat
+  return (digit, input.drop 1)
+
+def parseInteger (input : String) : Option (Float × String) := do
+  let (first, input) <- parseDigit input
+  if first == 0 then
+    return (0, input)
+  else
+    let mut current := first.toFloat
+    let mut input := input
+    repeat
+      let next? := parseDigit input
+      match next? with
+      | some (digit, input_) =>
+        current := current * 10 + digit.toFloat
+        input := input_
+      | none => break
+    return (current, input)
+
+def parseFraction (input : String) : Option (Float × String) := do
+  if input.startsWith "." then
+    let input := input.drop 1
+    let mut current := 0.0
+    let mut factor := 0.1
+    let mut input := input
+    repeat
+      let next? := parseDigit input
+      match next? with
+      | some (digit, input_) =>
+        current := current + factor * digit.toFloat
+        factor := factor / 10.0
+        input := input_
+      | none => break
+    return (current, input)
+  else
+    return (0.0, input)
+
+-- TODO: + is allowed in sign there, need to adapt.
+def parseExponent (input : String) : Option (Float × String) := do
+  if (input.startsWith "e" || input.startsWith "E") then
+    let input := input.drop 1
+    let (sign, input) <- parseSign input
+    let mut (integer, input) <- parseInteger input
+    repeat
+      let next? := parseDigit input
+      match next? with
+      | some (digit, input_) =>
+        integer := integer * 10 + digit.toFloat
+        input := input_
+      | none => break
+    return (10.0 ^ (sign * integer), input)
+  else
+    return (1.0, input)
+
+def parseNumber (input : String) : Option (Json × String) := do
+  let (sign, input) <- parseSign input
+  let (integer, input) <- parseInteger input
+  let (fraction, input) <- parseFraction input
+  let (exponent, input) <- parseExponent input
+  return (Json.number (sign * (integer + fraction) * exponent), input)
+
+def isHexDigit (c : Char) : Bool :=
+  ('0' ≤ c ∧ c ≤ '9') ∨
+  ('a' ≤ c ∧ c ≤ 'f') ∨
+  ('A' ≤ c ∧ c ≤ 'F')
+
+def hexCharToNat (c : Char) : Nat :=
+  if '0' ≤ c ∧ c ≤ '9' then
+    c.toNat - '0'.toNat
+  else if 'a' ≤ c ∧ c ≤ 'f' then
+    10 + (c.toNat - 'a'.toNat)
+  else if 'A' ≤ c ∧ c ≤ 'F' then
+    10 + (c.toNat - 'A'.toNat)
+  else
+    panic! s!"Invalid hex character: {c}"
+
+def hexStringToNat (s : String) : Nat :=
+  s.foldl (init := 0) fun acc c =>
+    let d := hexCharToNat c
+    acc * 16 + d
+
+#eval hexStringToNat "0041"
+-- 65
+
+-- TODO: deal with surrogate pairs???
+def parseString (input : String) : Option (Json × String) := do
+  guard (input.startsWith "\"")
+  let mut input := input.drop 1
+  let mut string := ""
+  repeat
+    let char <- input.get? 0
+    input := input.drop 1
+    match char with
+    | '"' =>
+      --input := input.drop 1
+      break
+    | '\\' =>
+      let next <- input.get? 0
+      match next with
+      | '"' => input := input.drop 1; string := string ++ "\""
+      | '\\' => input := input.drop 1; string := string ++ "\\"
+      | '/' => input := input.drop 1; string := string ++ "/"
+      | 'b' => input := input.drop 1; string := string ++ "\u232B"
+      | 'f' => input := input.drop 1; string := string ++ "\u240C"
+      | 'n' => input := input.drop 1; string := string ++ "\n"
+      | 'r' => input := input.drop 1; string := string ++ "\r"
+      | 't' => input := input.drop 1; string := string ++ "\t"
+      | 'u' =>
+        input := input.drop 1
+        let hex := input.take 4
+        if hex.length == 4 && hex.all isHexDigit then
+          input := input.drop 4
+          string := string ++ (hex |> hexStringToNat |> Char.ofNat |>.toString)
+        else
+          none -- failure
+      | _ => none -- failure
+    | _ => string := string ++ char.toString
+  return (Json.string string, input)
+
+#eval parseString r#""Hello!""#
+-- some (v9.Json.string "Hello!", "")
+
+#eval parseString r#""😊""#
+-- some (v9.Json.string "😊", "")
+
+#eval parseString r#""\n\r\t""#
+-- some (v9.Json.string "\n\x0d\t", "")
+
+#eval parseString r#""\u0041""#
+-- some (v9.Json.string "A", "")
+
+mutual
+
+  partial def parseValue (input : String) : Option (Json × String) := do
+    let input' := input.trimLeft
+    let (json, input'') <- Json.parse input'
+    return (json, input''.trimLeft)
+
+  -- one or more, comma-separated values (greedy)
+  partial def parseValues (input : String) : Option (List Json × String) := do
+    let (json, input) <- Json.parse input.trimLeft
+    let input := input.trimLeft
+    if input.startsWith "," then
+      let (elements, input) <- parseValues (input.drop 1)
+      return (json :: elements, input.trimLeft)
+    else
+      return ([json], input)
+
+  partial def parseArray (input : String) : Option (Json × String) := do
+    guard (input.startsWith "[")
+    let input' := (input.drop 1).trimLeft
+    if input'.startsWith "]" then
+        return (Json.array [], input'.drop 1)
+    let (elements, input'') <- parseValues input'
+    guard (input''.startsWith "]")
+    return (Json.array elements, input''.drop 1)
+
+  partial def parseMembers (input : String) : Option (List (String × Json) × String) := do
+    let mut members := []
+    let mut input := input.trimLeft
+    repeat
+      let (key_json, input') <- parseString input
+      let key <- match key_json with
+        | Json.string s => some s
+        | _ => none -- failure
+      input := input'.trimLeft
+      guard (input.startsWith ":")
+      input := (input.drop 1).trimLeft
+      let (value, input') <- Json.parse input
+      input := input'.trimLeft
+      members := (key, value) :: members
+      if !input.startsWith "," then
+        break
+      input := (input.drop 1).trimLeft
+    return (members.reverse, input)
+
+  partial def parseObject (input : String) : Option (Json × String) := do
+    guard (input.startsWith "{")
+    let input := (input.drop 1).trimLeft
+    if input.startsWith "}" then
+      return (Json.object [], input.drop 1)
+    let (members, input) <- parseMembers input
+    guard (input.startsWith "}")
+    return (Json.object members, input.drop 1)
+
+  partial def Json.parse (input : String) : Option (Json × String) :=
+    parseNull input
+    <|> parseBool input
+    <|> parseNumber input
+    <|> parseString input
+    <|> parseArray input
+    <|> parseObject input
+end
+
+#eval parseMembers r#""a": 1, "b": 2, "c": 3"#
+-- some ([("a", v9.Json.number 1.000000), ("b", v9.Json.number 2.000000), ("c", v9.Json.number 3.000000)], "")
+
+def parseEverything (parser : String → Option (Json × String)) (input : String) : Option Json :=
+  match parser input with
+  | some (json, rest) => if rest == "" then some json else none
+  | none => none
+
+#eval parseValue r#"{"a": 1}"#
+-- some (v9.Json.object [("a", v9.Json.number 1.000000)], "")
+
+#eval parseValue r#"{"a": 1, "b": 2}"#
+-- some (v9.Json.object [("a", v9.Json.number 1.000000), ("b", v9.Json.number 2.000000)], "")
+
+#eval parseValue r#"{"a": 1, "b": 2, "c": 3}"#
+-- some (v9.Json.object [("a", v9.Json.number 1.000000), ("b", v9.Json.number 2.000000), ("c", v9.Json.number 3.000000)], "")
+
+#eval parseValue r#"{"a": 1, "b": "Hello!", "c": true, "d": {}}"#
+-- some (v9.Json.object
+--    [("a", v9.Json.number 1.000000), ("b", v9.Json.string "Hello!"), ("c", v9.Json.bool true), ("d", v9.Json.object [])],
+--  "")
+
+end v9
