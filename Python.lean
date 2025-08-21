a4640251a6681937d4c85a53bd5010c4f00344c6
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-08-21 18:47:16 +0200

    Add batching to Python

--- Python.lean
+++ Python.lean
@@ -27,11 +27,33 @@ def exec_output! (code : String) : IO String := do
     panic! s!"Command failed with exit code {out.exitCode}: {out.stderr}"
   return out.stdout
 
+-- IO execution at the global level, see:
+-- https://lean-lang.org/doc/reference/latest/Elaboration-and-Compilation/
+initialize shouldBatch : IO.Ref Bool <- IO.mkRef false
+initialize todos : IO.Ref (List String) <- IO.mkRef []
+
+def flush! : IO Unit := do
+  let codes <- todos.get
+  if not codes.isEmpty then
+    let code := "\n".intercalate codes
+    discard (exec_output! code)
+
+def batch {α} (action : IO α) : IO α := do
+  shouldBatch.set true
+  let result <- action
+  flush!
+  shouldBatch.set false
+  return result
+
 def exec! (code : String) : IO Unit := do
-  let _ <- exec_output! code
-  return ()
+  if (<- shouldBatch.get) then
+    todos.modify (. ++ [code])
+  else
+    flush!
+    discard (exec_output! code)
 
 def eval! (code : String) : IO String := do
+  flush!
   exec_output! s!"_ = {code}; print(_, end='', flush=True)"
 
 end Python
