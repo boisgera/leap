0e3b8a758cb9562424813b1728dea6478739c30a
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-08-28 12:27:09 +0200

    Extra except stuff

--- sandbox.lean
+++ sandbox.lean
@@ -124,6 +124,8 @@ def f (e : E) (h : ∃ (n : Nat), e = .main n) : Nat :=
 -- a generic extractor), so that the final user never has to consider a
 -- pattern matching that cannot happen.
 
+-- -----------------------------------------------------------------------------
+
 def reportAux (dna : String) : Except DecodeError String := do
     try
       let l <- decodeDNA dna
@@ -131,36 +133,47 @@ def reportAux (dna : String) : Except DecodeError String := do
     catch e =>
       return s!"❌ invalid character '{e.char}' at position {e.pos}"
 
-theorem reportAuxCantFail :
-  ∀ (dna : String), (reportAux dna).isOk = true := by
-    intro dna
-    rw [Except.isOk]
-    rw [Except.toBool.eq_def]
-    rw [reportAux]
+theorem safe_try_except {ε α} (body : Except ε α) (handler : ε -> Except ε α) :
+  (∀ (e : ε), (handler e).isOk = true) -> (tryCatch body handler).isOk = true
+  := by
+  intro h_ok
+  rw [tryCatch]
+  rw [instMonadExceptOfMonadExceptOf]
+  simp [tryCatchThe, MonadExceptOf.tryCatch, Except.tryCatch]
+  cases body with
+  | ok a =>
+    rw [Except.isOk, Except.toBool]
+  | error e =>
     simp
-    -- Argh, need to decompose the match somehow ...
-    /-
-    ⊢ (match
-        tryCatch ((fun a => toString "✅ decoded sequence of length " ++ toString a.length ++ toString "") <$> decodeDNA dna)
-          fun e =>
-          pure
-            (toString "❌ invalid character '" ++ toString e.char ++ toString "' at position " ++ toString e.pos ++
-              toString "") with
-      | Except.ok a => true
-      | Except.error a => false) =
-      true
-    -/
-    simp [tryCatch, tryCatchThe, MonadExceptOf.tryCatch]
-    admit
+    rw [h_ok e]
 
-def report' (dna : String) : String :=
+theorem reportAuxCantFail : ∀ (dna : String), (reportAux dna).isOk = true := by
+  intro dna
+  simp [reportAux]
+  apply safe_try_except
+  simp [Except.isOk, Except.toBool, pure, Except.pure]
+
+def Except.get {ε α} (except : Except ε α) : except.isOk = true -> α :=
+  fun ex_ok =>
+    match except with
+    | ok a => a
+    | error _ => nomatch ex_ok
+
+def report' := fun (dna : String) =>
+  Except.get (reportAux dna) (reportAuxCantFail dna)
+
+
+-- -----------------------------------------------------------------------------
+
+
+def report'' (dna : String) : String :=
   let message? := reportAux dna
   match message? with
   | .ok message => message
   | .error _ => panic! "unreachable!"
 
-#eval report' "GATTACA"
+#eval report'' "GATTACA"
 -- "✅ decoded sequence of length 7"
 
-#eval report' "TARATATA"
+#eval report'' "TARATATA"
 -- "❌ invalid character 'R' at position 2"
