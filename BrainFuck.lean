83c8e080c373c9b618db1bad632f53b3bcfcdf86
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-09-17 21:13:30 +0200

    Start brainfuck

---
+++ BrainFuck.lean
@@ -0,0 +1,129 @@
+/-
+TODO:
+
+  - baby brainfuck, simplified as much as possible?
+    Boolfuck? HQ9+? 1L (https://esolangs.org/wiki/1L_a)?
+
+  - brainfuck by steps: no IO first but return data at the end (?)
+    no "[" and "]"" at first?
+
+  - need testing
+
+  - need parser (with characters others than listed ignored)
+
+  - add a "dump" instruction?
+
+  - improve read/write wrt to non-printable characters
+
+  - Thoughts: the structure with IO for everything sucks. I don't mind eval
+    being in IO but I don't like that for next. I'd rather add an optional
+    IO Unit field to the state and handle it in the eval loop.
+
+  - Then use the State monad somehow when that's cleaned up?
+
+  - lift more methods to the Tape and Prg? Encapsulate indices in Tape and
+    Prg?
+
+  - Spoon binary encoding/decoding?
+
+-/
+
+namespace BrainFuck
+
+abbrev Byte := UInt8
+
+/-
+An infinite read-write binary data tape initialized with zeroes
+-/
+
+def Tape := List Byte deriving Repr
+
+def Tape.mk (bytes : List Byte) : Tape := bytes
+
+def Tape.toList (tape : Tape): List Byte := tape
+
+def Tape.get (tape : Tape) (i : Nat) : Byte :=
+  if h : i <  tape.length then
+    tape.toList[i]
+  else
+    0
+
+def Tape.set (tape : Tape) (i : Nat) (byte : Byte) : Tape :=
+  let tape := if i < tape.length then
+    tape
+  else
+    tape.toList ++ (List.replicate (i - tape.length + 1) 0)
+  tape.toList.set i byte
+
+#eval Tape.mk []
+-- []
+
+#eval Tape.mk [1, 2, 3] |>.get 2
+-- 3
+
+#eval Tape.mk [1, 2, 3] |>.get 3
+-- 0
+
+#eval Tape.mk [1, 2, 3] |>.set 2 4
+-- [1, 2, 4]
+
+#eval Tape.mk [1, 2, 3] |>.set 6 4
+-- [1, 2, 3, 0, 0, 0, 4]
+
+inductive Instruction where
+  | shiftRight
+  | shiftLeft
+  | increment
+  | decrement
+  | write
+  | read
+  | jumpForward
+  | jumpBackward
+--  | dump
+--  | exit
+deriving Repr, Inhabited
+
+abbrev Program := List Instruction
+
+/-
+Execution state is: Some program, an instruction pointer (optional) some data (optional), some data pointer (optional)
+-/
+
+structure State where
+  prg : Program
+  i: Nat := 0 -- instruction pointer
+  data: Tape := []
+  j : Nat := 0 -- data pointer
+
+
+def next (state : State) : IO State := do
+  match state.prg[state.i]! with
+  | .shiftRight => return { state with i := state.i + 1, j := state.j + 1 }
+  | .shiftLeft  => return { state with i := state.i + 1, j := state.j - 1 }
+  | .increment  => return { state with i := state.i + 1, data := state.data.get state.j |> (· + 1) |> state.data.set state.j }
+  | .decrement  => return { state with i := state.i + 1, data := state.data.get state.j |> (· - 1) |> state.data.set state.j }
+  | .write => -- TODO: improve when not printable? (e.g. use \x??)
+    IO.print (Char.ofNat (state.data.get state.j).toNat)
+    return { state with i := state.i + 1 }
+  | .read => -- TODO: improve to deal with non-printable chars.
+    let stdin <- IO.getStdin
+    let line <- stdin.getLine
+    let char := line.front
+    let number := char.toUInt8
+    return { state with i := state.i + 1 , data := state.data |>.set state.j number }
+  | .jumpForward =>
+    let i' := (state.prg.drop (state.i + 1)).findIdx (· matches .jumpBackward)
+    return { state with i := (state.i + 1) + i' + 1 }
+  | .jumpBackward =>
+    let i' := state.prg.take state.i |>.findIdx (· matches .jumpForward)
+    return { state with i := i' + 1 }
+
+partial def eval (state : State) : IO Unit := do
+  if state.i < state.prg.length then
+    eval (<- next state)
+
+
+
+
+
+end BrainFuck
