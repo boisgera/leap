e0625da397fcc041690b4ffea436585e39a48bd5
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-11-22 23:19:16 +0100

    Work in progress Lecture 7

--- RealAnalysis/07.lean
+++ RealAnalysis/07.lean
@@ -43,3 +43,31 @@ SeqLim a ℓ₁ -> SeqLim a ℓ₂ -> ℓ₁ = ℓ₂ := by
   have : |ℓ₁ - ℓ₂| = 0 :=
     lemma₁ (show |ℓ₁ - ℓ₂| >= 0 from abs_nonneg (ℓ₁ - ℓ₂)) h₃
   exact eq_of_abs_sub_eq_zero this
+
+#check abs_sub_abs_le_abs_sub
+#check abs_neg
+
+theorem eventually_greater_than_half_the_limit (a : ℕ -> ℝ) (ℓ : ℝ):
+  SeqLim a ℓ -> ℓ ≠ 0 ->
+  ∃ N : ℕ, ∀ n : ℕ, n >= N -> |a n| >= |ℓ| / 2 := by
+  intro seq_lim_a_ℓ ℓ_nonzero
+  rw [SeqLim] at seq_lim_a_ℓ
+  have half_abs_ℓ_pos: |ℓ| / 2 > 0 := by
+    have : |ℓ| > 0 := abs_pos.mpr ℓ_nonzero
+    linarith
+  specialize seq_lim_a_ℓ (|ℓ| / 2) half_abs_ℓ_pos
+  have ⟨N, hN⟩ := seq_lim_a_ℓ; clear seq_lim_a_ℓ
+  use N
+  intro n n_ge_N
+  specialize hN n n_ge_N
+  have hN' : |ℓ - a n| < |ℓ| / 2 := by
+    calc |ℓ - a n|
+      _ = |-(a n - ℓ)| := by ring_nf
+      _ = |a n - ℓ| := by rw [abs_neg]
+      _ < |ℓ| / 2 := hN
+  have : |ℓ| - |a n| <= |ℓ - a n| :=
+    abs_sub_abs_le_abs_sub ℓ (a n)
+  have : |ℓ| - |a n| < |ℓ| / 2 :=
+    lt_of_le_of_lt this hN'
+  have : |ℓ| - |a n| <= |ℓ| / 2 := le_of_lt this
+  linarith
