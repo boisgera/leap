e770f3dea9222374a610442cd9feeb23a30da9d2
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-11-29 23:22:12 +0100

    Induction

--- RealAnalysis/08.lean
+++ RealAnalysis/08.lean
@@ -1,5 +1,24 @@
 import Mathlib
 
+example : ∀ n, 1 ≤ 2 ^ n := by
+  intro n
+  induction n with
+  | zero => norm_num
+  | succ n ih =>
+      simp [pow_add, pow_one]
+      linarith
+
 example : ∀ n : ℕ, n < 2 ^ n := by
-  -- TODO!
-  admit
+  intro n
+  induction n with
+  | zero => norm_num
+  | succ n ih =>
+      rw [pow_add, pow_one]
+      linarith
+
+example : ∀ n : ℕ, n < 2 ^ n := by
+  intro n
+  induction' n with n ih
+  . norm_num
+  . rw [pow_add, pow_one]
+    linarith
