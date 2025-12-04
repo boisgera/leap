14a14d7b96b4769f13b83db0432aa5958c92ea46
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-12-04 18:55:02 +0100

    Add new limit statement

--- RealAnalysis/09.lean
+++ RealAnalysis/09.lean
@@ -168,9 +168,7 @@ theorem every_converging_sequence_is_bounded
       grind only [= mem_range, = max_def, cases Or]
     linarith
 
--- An inequality used to show that analytic function is differentiable:
-
-lemma ineq (z h : ℂ) (h_neq_zero : h ≠ 0) (n : ℕ) (n_ge_two : n ≥ 2) :
-‖((z + h) ^ n - z ^n) / h - n * z ^ (n - 1)‖ ≤
-n * (n - 1) / 2 * (‖z‖ + ‖h‖) ^ (n - 2) * ‖h‖ := by
-  admit
+theorem prod_of_convergent_sequences
+(a b : ℕ → ℝ) (ℓ ℓ' : ℝ)
+(seq_lim_a_ℓ : SeqLim a ℓ) (seq_lim_b_ℓ' : SeqLim b ℓ') :
+SeqLim (fun n => a n * b n) (ℓ * ℓ') := by admit
