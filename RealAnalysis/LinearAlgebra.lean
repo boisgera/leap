a96487b7bef05f5065861443b3c795c16e61e210
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2025-12-09 23:55:06 +0100

    Linear Continuous Maps (fin. dim.)

--- RealAnalysis/LinearAlgebra.lean
+++ RealAnalysis/LinearAlgebra.lean
@@ -50,3 +50,35 @@ def fl' : (Fin 2 → ℝ) →ₗ[ℝ] (Fin 3 → ℝ) where
 -- ![Real.ofCauchy (sorry /- 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ... -/),
 --  Real.ofCauchy (sorry /- 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, ... -/),
 --  Real.ofCauchy (sorry /- 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, ... -/)]
+
+#check ContinuousLinearMap
+
+def f_lin_cont : (Fin 2 → ℝ) →L[ℝ] (Fin 3 → ℝ) :=
+  let f := fun x => ![x 0, x 1, x 0 + x 1]
+  let f_lin : (Fin 2 → ℝ) →ₗ[ℝ] (Fin 3 → ℝ) := {
+    toFun := f
+    map_add' := by intro x y ; simp [f] ; ring
+    map_smul' := by intro m x ; simp [f] ; ring
+  }
+  let f_lin_cont : (Fin 2 → ℝ) →L[ℝ] (Fin 3 → ℝ) := {
+    f_lin with
+    cont := LinearMap.continuous_of_finiteDimensional f_lin
+  }
+  f_lin_cont
+
+def f_lin_cont'' : (Fin 2 → ℝ) →L[ℝ] (Fin 3 → ℝ) :=
+  {
+    toFun := fun x => ![x 0, x 1, x 0 + x 1]
+    map_add' := by intro x y ; simp ; ring
+    map_smul' := by intro m x ; simp ; ring
+    : (Fin 2 → ℝ) →ₗ[ℝ] (Fin 3 → ℝ)
+  } |>.toContinuousLinearMap
+
+def f_lin_cont''' : (Fin 2 → ℝ) →L[ℝ] (Fin 3 → ℝ) :=
+  (
+    {
+    toFun := fun x => ![x 0, x 1, x 0 + x 1]
+    map_add' := by intro x y ; simp ; ring
+    map_smul' := by intro m x ; simp ; ring
+    } : (Fin 2 → ℝ) →ₗ[ℝ] (Fin 3 → ℝ)
+  ) |>.toContinuousLinearMap
