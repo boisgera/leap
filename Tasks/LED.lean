6706f24f3025c754eef4bb01a9d2174551547594
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-01-15 21:18:24 +0100

    Refactor LED function and add TrafficLight module with recursive traffic light handling

--- Tasks/LED.lean
+++ Tasks/LED.lean
@@ -1,8 +1,35 @@
-def LED (symbol : String := "🔵") (ms : UInt32 := 1000) : IO Unit := do
+def LED (symbol : String) (ms : UInt32 := 1000) : IO Unit := do
   repeat
     IO.println symbol
     IO.sleep ms
 
+inductive TrafficLight where
+| green
+| orange
+| red
+
+def TrafficLight.symbol (tf : TrafficLight) : String :=
+  match tf with
+  | green => "🟢"
+  | orange => "🟠"
+  | red => "🔴"
+
+def TrafficLight.rotate (tf : TrafficLight) : TrafficLight :=
+  match tf with
+  | green => orange
+  | orange => red
+  | red => green
+
+-- The function can also call itself recursively to avoid using a repeat loop.
+partial def trafficLight (tf : TrafficLight) (ms : UInt32 := 1000) : IO Unit := do
+  IO.println tf.symbol
+  IO.sleep 1000
+  trafficLight tf.rotate ms
+
 def main : IO Unit := do
-  let _ <- IO.asTask <| LED "⚪" 1000
-  let _ <- IO.asTask <| LED "⚫" 500
+  -- Here what matters is that the tasks start;
+  -- the task objects themselves can be discarded.
+  let _ <- IO.asTask (LED "⚪") -- or: discard (IO.asTask (LED "⚪"))
+  let _ <- IO.asTask (LED "⚫" (ms := 500))
+  let _ <- IO.asTask (trafficLight .green)
+  -- The program doesn't stop until all threads are done.
