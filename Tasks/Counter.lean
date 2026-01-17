5abff5b13c85640e3a9d22520f1fc2060d58211d
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-01-17 14:23:44 +0100

    Add Counter structure and initialization function

--- Tasks/Counter.lean
+++ Tasks/Counter.lean
@@ -1,5 +1,19 @@
 import Std.Sync.Channel
 
+structure Counter where
+  count : Int
+  c_add : IO (Std.Channel Int)
+
+
+def Counter.new (count : Int := 0) : IO Counter :=
+  let actor : Counter := {
+    count := count,
+    c_add := Std.Channel.new (α := Int)
+  }
+  /- Mmm we need to start the listening loop here! -/
+
+  return actor
+
 partial def counter (c : Std.Channel Int) (count : Int) : IO Unit := do
   let extra := (<- c.recv).get
   counter c (count + extra)
