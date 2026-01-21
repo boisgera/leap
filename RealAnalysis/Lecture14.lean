bb511e8ba2f2974583c9e2c861614d7f2ea33c7e
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-01-21 12:05:49 +0100

    Snapshot

--- RealAnalysis/Lecture14.lean
+++ RealAnalysis/Lecture14.lean
@@ -13,6 +13,31 @@ def BolzanoWeierstrass (a : ℕ → ℝ) :
 -- strictMono_or_antitone_subsequence (a : ℕ → ℝ) :
 --     ∃ b, SubSeq b a ∧ (StrictMono b ∨ Antitone b)
 
+lemma Monotone.isCauchy_iff {a : ℕ → ℝ} (monotone_a : Monotone a) :
+    IsCauchy a ↔ ∀ ε > 0, ∃ m, ∀ n ≥ m, ∀ p > n, a p - a n < ε := by
+  apply Iff.intro
+  . intro isCauchy
+    rw [IsCauchy] at isCauchy
+    intro ε ε_pos
+    let ⟨m, hm⟩ := isCauchy ε ε_pos
+    use m
+    intro n n_ge_m p p_gt_n
+    specialize hm n n_ge_m p (show p ≥ m from by omega)
+    have : a p ≥ a n := by apply monotone_a ; grind
+    have : |a n - a p| = a p - a n := by grind
+    rw [this] at hm
+    exact hm
+  . intro h
+    rw [IsCauchy]
+    intro ε ε_pos
+    specialize h ε ε_pos
+    have ⟨m, hm⟩ := h
+    use m
+    intro n n_ge_m p p_ge_m
+    -- we need a trichotomy here : n < p, n = p or n > p and do some
+    -- case analysis, using hm in two different ways.
+    admit
+
 theorem IsCauchy_of_monotone_and_upperBound (a : ℕ → ℝ) :
     Monotone a -> (∃ ub, ∀ n, a n ≤ ub) -> IsCauchy a := by
   intro monotone_a ⟨ub, a_n_le_ub⟩
