5f6581018bcd644fdb64df07c4696693d547d50f
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-02-28 11:55:40 +0100

    Sets

--- MIL/04 - Sets and Functions.lean
+++ MIL/04 - Sets and Functions.lean
@@ -172,30 +172,211 @@ example {α} {s : Set α} : s = Set.univ ↔ ∀ x, x ∈ s := by
 -- Set.mem_univ.{u} {α : Type u} (x : α) : x ∈ Set.univ
 
 
--- -----------------------------------------------------------------------------
 
--- Insert stuff and {a, b, c} notation
+-- ## `Singleton`, `Insert` and `{a, b, c}` notation
 
-def singleton_one : Set ℕ := {1}
--- Set.mem_insert.{u} {α : Type u} (x : α) (s : Set α) : x ∈ insert x s
+#check Set.singleton
+-- Set.singleton.{u} {α : Type u} (a : α) : Set α
 
+#print Set.singleton
+-- protected def Set.singleton.{u} : {α : Type u} → α → Set α :=
+-- fun {α} a => {b | b = a}
+
+-- The notation that you should use for a singleton is `{x}`.
+
+example {α} (x : α): {x} = (Set.singleton x) := by rfl
+
+-- I was a bit suprised that the definition was not using the insertion into
+-- and empty set but actually I am advised not to use this definition but
+-- instead:
+
+#check Set.singleton_def
+-- Set.singleton_def.{u} {α : Type u} (a : α) : {a} = insert a ∅
+
+-- ... so that kinda solves that. Let's say that how `Set.singleton` is defined
+-- is an implementation detail.
+
+
+#check Set.insert
+-- Set.insert.{u} {α : Type u} (a : α) (s : Set α) : Set α
+
+-- I am a bit baffled by the order of the arguments here, I guess that I
+-- would rather have the set first and the item second. But not a big deal
+-- I guess since I can use the uniform function call syntax (UFCS) to
+-- have `s.insert a` and/or `|>.insert` to chain multiple inserts.
+-- OTOH the current convention matches the `List.cons` approach...
+
+#print Set.insert
+-- protected def Set.insert.{u} : {α : Type u} → α → Set α → Set α :=
+-- fun {α} a s => {b | b = a ∨ b ∈ s}
 
 #check Set.insert_def
--- Set.insert_def.{u} {α : Type u} (x : α) (s : Set α) : insert x s = {y | y = x ∨ y ∈ s}
-
-#check Set.mem_insert
--- Set.mem_insert.{u} {α : Type u} (x : α) (s : Set α) : x ∈ insert x s
-
-example (m n : ℕ) : m ∈ ({n} : Set ℕ) ↔ m = n := by
-  simp only [Set.insert_def]
-  admit
+-- Set.insert_def.{u} {α : Type u} (x : α) (s : Set α) :
+-- insert x s = {y | y = x ∨ y ∈ s}
 
 
+example {α} (x : α) : {x} = Set.insert x {} := by
+  simp only [Set.singleton_def]
+  -- insert x ∅ = Set.insert x ∅
+  rfl
 
-def zot : Set ℕ := {0, 1, 2}
+-- That's actually similar to `Set.eq_of_mem_singleton` in Mathlib
+example {α} (x y : α) : x ∈ ({y} : Set α) ↔ x = y := by
+  simp only [
+    Set.singleton_def,
+    Set.insert_def,
+    Set.mem_empty_iff_false,
+    or_false,
+    Set.mem_setOf
+  ]
 
-def large : Set ℕ := { n : ℕ | n ≥ 1000 }
+#check Set.eq_of_mem_singleton
+-- Set.eq_of_mem_singleton.{u} {α : Type u} {x y : α} (h : x ∈ {y}) : x = y
 
-def allN : Set ℕ := Set.univ
+example {α} (a : α) : {a, a} = ({a} : Set α) := by
+  rw [Set.singleton_def]
+  repeat rw [Set.insert_def]
+  simp only [Set.mem_setOf, Set.mem_empty_iff_false, or_false, or_self]
 
--- -----------------------------------------------------------------------------
+example {α} (a b : α) : {a, b} = ({b, a} : Set α) := by
+  simp only [Set.insert_def, Set.singleton_def]
+  simp only [Set.mem_empty_iff_false, or_false, Set.mem_setOf]
+  ext x; simp only [Set.mem_setOf]
+  -- ⊢ x = a ∨ x = b ↔ x = b ∨ x = a
+  simp only [or_comm]
+
+
+-- ## Unary and Binary Operators
+
+#check Set.compl_def
+-- Set.compl_def.{u_1} {α : Type u_1} (s : Set α) : sᶜ = {x | x ∉ s}
+
+#check Set.union_def
+-- Set.union_def.{u} {α : Type u} {s₁ s₂ : Set α} : s₁ ∪ s₂ = {a | a ∈ s₁ ∨ a ∈ s₂}
+
+#check Set.inter_def
+-- Set.inter_def.{u} {α : Type u} {s₁ s₂ : Set α} : s₁ ∩ s₂ = {a | a ∈ s₁ ∧ a ∈ s₂}
+
+#check Set.diff_eq
+-- Set.diff_eq.{u} {α : Type u} (s t : Set α) : s \ t = s ∩ tᶜ
+
+#check Set.symmDiff_def
+-- Set.symmDiff_def.{u} {α : Type u} (s t : Set α) : symmDiff s t = s \ t ∪ t \ s
+
+example {α} (s : Set α) : (sᶜ)ᶜ = s := by
+  ext x
+  simp only [Set.compl_def, Set.mem_setOf]
+  simp only [not_not]
+
+#check compl_compl
+-- compl_compl.{u} {α : Type u} [BooleanAlgebra α] (x : α) : xᶜᶜ = x
+
+example {α} : Set.univᶜ = (∅ : Set α) := by
+  -- Note: univ is "public" in the Set type class, empty is not even there.
+  simp only [Set.univ, Set.empty_def]
+  rw [Set.compl_def]
+  simp only [Set.mem_setOf, not_true]
+
+#check Set.compl_univ
+-- Set.compl_univ.{u_1} {α : Type u_1} : Set.univᶜ = ∅
+
+example {α} : (∅ᶜ : Set α) = Set.univ := by
+  simp only [Set.compl_def, Set.univ, Set.empty_def, Set.mem_setOf]
+  simp only [not_false_iff]
+
+#check Set.compl_empty
+-- Set.compl_empty.{u_1} {α : Type u_1} : ∅ᶜ = Set.univ
+
+example {α} (s t : Set α) : s ∪ t = t ∪ s := by
+  simp only [Set.union_def]
+  simp only [or_comm]
+
+#check Set.union_comm
+-- Set.union_comm.{u} {α : Type u} (a b : Set α) : a ∪ b = b ∪ a
+
+example {α} {s t : Set α} : (s ∪ t)ᶜ = sᶜ ∩ tᶜ := by
+  simp only [Set.union_def, Set.inter_def, Set.compl_def]
+  ext x; simp only [Set.mem_setOf]
+  -- ⊢ ¬(x ∈ s ∨ x ∈ t) ↔ x ∉ s ∧ x ∉ t
+  simp only [not_or]
+
+#check Set.compl_union
+-- Set.compl_union.{u_1} {α : Type u_1} (s t : Set α) : (s ∪ t)ᶜ = sᶜ ∩ tᶜ
+
+example {α} (r s t : Set α) : r ∪ (s ∩ t) = (r ∪ s) ∩ (r ∪ t) := by
+  simp only [Set.union_def, Set.inter_def, Set.mem_setOf]
+  ext x; simp only [Set.mem_setOf]
+  -- ⊢ x ∈ r ∨ x ∈ s ∧ x ∈ t ↔ (x ∈ r ∨ x ∈ s) ∧ (x ∈ r ∨ x ∈ t)
+  simp [or_and_left]
+
+#check Set.union_inter_distrib_left
+-- Set.union_inter_distrib_left.{u} {α : Type u} (s t u : Set α) :
+-- s ∪ t ∩ u = (s ∪ t) ∩ (s ∪ u)
+
+example {α} (r s t : Set α) : (r \ s) \ t = r \ (s ∪ t) := by
+  simp only [Set.diff_eq, Set.inter_def, Set.compl_def, Set.union_def]
+  simp only [Set.mem_setOf]
+  ext x; simp only [Set.mem_setOf]
+  simp_all only [not_or]
+  simp only [and_assoc]
+
+-- ## Inclusion and Power Set
+
+#check Set.subset_def
+-- Set.subset_def.{u} {α : Type u} {s t : Set α} : (s ⊆ t) = ∀ x ∈ s, x ∈ t
+
+example {α} (r s : Set α) : r ⊆ r ∪ s := by
+  simp only [Set.subset_def]
+  intro x x_in_r
+  simp only [Set.mem_union]
+  left
+  assumption
+
+-- Power set 𝒫 : AFAICT, the set of all subsets ** of a set, not a type**.
+-- So 𝒫 ℕ is meaningless, but 𝒫 (Set.univ (α := ℕ)) makes sense for example.
+
+#check 𝒫  {n : ℕ | n < 100}
+-- 𝒫 {n | n < 100} : Set (Set ℕ)
+
+#print Set.powerset
+-- def Set.powerset.{u} : {α : Type u} → Set α → Set (Set α) :=
+-- fun {α} s => {t | t ⊆ s}
+
+example {α} (r s : Set α): r ∈ 𝒫 s ↔ r ⊆ s  := by
+  simp only [Set.powerset, Set.mem_setOf]
+
+-- ## Union and Intersection of Collections/Families
+
+#check Set.sUnion
+-- Set.sUnion.{u} {α : Type u} (S : Set (Set α)) : Set α
+
+-- Don't go the route of the definition of this stuff, this goes too abstract
+-- (IMHO). Instead use the operational theorem:
+
+#check Set.mem_sUnion
+-- Set.mem_sUnion.{u} {α : Type u} {x : α} {S : Set (Set α)} :
+-- x ∈ ⋃₀ S ↔ ∃ t ∈ S, x ∈ t
+
+example {α} : ⋃₀ (Set.univ : Set (Set α)) = (Set.univ : Set α) := by
+  ext x
+  simp only [Set.mem_sUnion, Set.mem_univ]
+  simp only [true_and, iff_true]
+  use Set.univ
+  simp only [Set.mem_univ]
+
+#check Set.iUnion
+-- Set.iUnion.{u, v} {α : Type u} {ι : Sort v} (s : ι → Set α) : Set α
+
+#check Set.mem_iUnion
+-- Set.mem_iUnion.{u, v} {α : Type u} {ι : Sort v} {x : α} {s : ι → Set α} :
+-- x ∈ ⋃ i, s i ↔ ∃ i, x ∈ s i
+
+example {α} : ⋃ x : α, {x} = Set.univ := by
+  ext y
+  simp only [Set.mem_iUnion, Set.mem_univ]
+  simp only [iff_true]
+  use y; simp only [Set.mem_singleton_iff]
+
+
+-- **TODO.** Finsets and stuff
+-- **TODO.** coutable union and stuff (think σ-algebra axioms).
