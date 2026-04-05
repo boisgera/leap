59db2a365e186f90329a88c81e32ff1c91028d2b
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-04-06 00:37:12 +0200

    Snapshot

--- RealAnalysis/Lecture21.lean
+++ RealAnalysis/Lecture21.lean
@@ -29,32 +29,52 @@ lemma f_factorization (x x_0 : ℝ) :
   ring_nf
 
 lemma f_diff_domination (x x_0 δ : ℝ) (δ_pos : δ > 0) :
-    |x - x_0| ≤ δ -> |f x - f x_0| ≤ δ * (δ + 2 * |x_0|) := by
+    |x - x_0| < δ -> |f x - f x_0| < δ * (δ + 2 * |x_0|) := by
   simp only [f_factorization]
   intro h
   simp only [abs_mul]
-  have hadd : |x - x_0 + 2 * x_0| ≤ |x - x_0| + |2 * x_0| :=
-    abs_add_le _ _
-  have hprod : |x - x_0| * |x - x_0 + 2 * x_0| ≤ |x - x_0| ^ 2 + |x - x_0| * (|2| * |x_0|) := by
+  have hadd : |x - x_0 + 2 * x_0| ≤ |x - x_0| + 2 * |x_0| := by
     have := abs_add_le (x - x_0) (2 * x_0)
-    simp only [abs_mul] at this
-    nlinarith [abs_nonneg (x - x_0), abs_nonneg (x - x_0 + 2 * x_0)]
-  have hsq : |x - x_0| ^ 2 ≤ δ ^ 2 := by nlinarith [abs_nonneg (x - x_0)]
-  have hmix : |x - x_0| * (|2| * |x_0|) ≤ δ * (|2| * |x_0|) :=
-    mul_le_mul_of_nonneg_right h (by positivity)
-  have h2 : |2| = (2 : ℝ) := by norm_num
-  rw [h2] at hprod hmix
-  nlinarith
+    simp only [abs_mul, abs_two] at this
+    linarith
+  have hsq : |x - x_0| ^ 2 < δ ^ 2 :=
+    sq_lt_sq' (by linarith [abs_nonneg (x - x_0)]) h
+  nlinarith [
+    abs_nonneg (x - x_0),
+    abs_nonneg x_0,
+    mul_nonneg (abs_nonneg (x - x_0)) (abs_nonneg (x - x_0 + 2 * x_0))
+  ]
 
 theorem continuousAt_everywhere (x : ℝ) : ContinuousAt f x := by
   simp only [Metric.continuousAt_iff, Real.dist_eq]
   intro ε ε_pos
-  let δ := min 1 ε / (1 + 2 * |x|)
-  have : δ > 0 := by positivity
-  have : δ ≤ 1 := by grind
-  have : δ ≤ ε / (1 + 2 * |x|) := by grind
+  let δ := min 1 (ε / (1 + 2 * |x|))
   use δ
-  admit
+  constructor
+  positivity
+  intro x_1
+  have dom := f_diff_domination x_1 x δ (by positivity)
+  intro h
+  specialize dom h
+  have ineq : δ * (δ + 2 * |x|) ≤ ε := by
+    have : δ * (δ + 2 * |x|) ≤ ε / (1 + 2 * |x|) * (1 + 2 * |x|) := by
+      apply mul_le_mul
+      . apply min_le_right
+      . linarith [show δ ≤ 1 from by apply min_le_left]
+      . positivity
+      . positivity
+    simp only [div_mul_cancel₀ ε (show (1 + 2 * |x|) ≠ 0 from by grind)] at this
+    exact this
+  linarith
+
+#check continuous_iff_continuousAt
+-- continuous_iff_continuousAt.{u_1, u_2} {X : Type u_1} {Y : Type u_2}
+-- [TopologicalSpace X] [TopologicalSpace Y] {f : X → Y} :
+-- Continuous f ↔ ∀ (x : X), ContinuousAt f x
+
+theorem continuous : Continuous f :=
+  continuous_iff_continuousAt.mpr continuousAt_everywhere
+
 
 end Ex_0
 
