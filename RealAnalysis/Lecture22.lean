887c1e8e600e75a20ab150127e58e260c6c9fd8b
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-04-29 16:34:49 +0200

    Definition of the Riemann integral as a special case of box integral.

--- RealAnalysis/Lecture22.lean
+++ RealAnalysis/Lecture22.lean
@@ -106,13 +106,77 @@ Prove that f (x) = x is integrable on [a, b] for a < b:
 
 #check BoxIntegral.HasIntegral
 -- BoxIntegral.HasIntegral.{u, v, w} {ι : Type u} {E : Type v} {F : Type w}
---   [NormedAddCommGroup E] [NormedSpace ℝ E] [NormedAddCommGroup F] [NormedSpace ℝ F]
+--   [NormedAddCommGroup E] [NormedSpace ℝ E]
+--   [NormedAddCommGroup F] [NormedSpace ℝ F]
 --   [Fintype ι]
---   (I : BoxIntegral.Box ι) (l : BoxIntegral.IntegrationParams)
---   (f : (ι → ℝ) → E) (vol : BoxIntegral.BoxAdditiveMap ι (E →L[ℝ] F) ⊤) (y : F) : Prop
+--   (I : BoxIntegral.Box ι)
+--   (l : BoxIntegral.IntegrationParams)
+--   (f : (ι → ℝ) → E)
+--   (vol : BoxIntegral.BoxAdditiveMap ι (E →L[ℝ] F) ⊤)
+--   (y : F) : Prop
 
 /-!
-TODO: specialize to Riemann stuff in 1D, scalar-valued functions.
+Main parameter breakdown:
+
+- `ι : Type u`: the index type.
+  It determines the dimension of the space.
+  The space being integrated over is `ι → ℝ`
+  which represents `ℝⁿ` when `ι := Fin n`.
+  The typeclass `[Fintype ι]` ensures `ι` is finite.
+
+- `I : BoxIntegral.Box ι`. The domain of integration.
+  A `BoxIntegral.Box ι` is a nontrivial rectangular box in `ι → ℝ` with corners
+  `lower` and `upper : ι → ℝ`, satisfying `∀ i, lower i < upper i`.
+  It is interpreted as the product of half-open intervals `(lower i, upper i]`.
+  So for `ι = Fin n`, this is an axis-aligned box in `ℝⁿ`.
+  Note: the box is closed on the low side and open on the up side
+  (a simple convention which simplifies partition of boxes).
+
+- `l : BoxIntegral.IntegrationParams`. The choice of integration theory.
+  `IntegrationParams` is a structure holding 3 Boolean values used to define a
+  filter; it encodes eight different parameter sets, including those
+  corresponding to the Riemann integral, the Henstock-Kurzweil integral,
+  and the McShane integral. Passing different values of `l` lets a single
+  definition cover all three theories without code duplication.
+  The Riemann, Henstock, McShane, and Generalized Perron theories have predefined
+  values in `IntegrationParams`: `.Riemann`, `.Henstock`, `.McShane` and `.GP`.
+
+- `f : (ι → ℝ) → E`. The integrand.
+  It takes a point in the box and returns a value in the normed space `E`.
+
+- `vol : BoxIntegral.BoxAdditiveMap ι (E →L[ℝ] F) ⊤`. The volume measure.
+
+  A function which associate to any box in the integration space a
+  (linear, continuous) function from `E` to `F` and is box-additive
+  It is a box-additive map: given a box `J` and a partition `π` of it,
+  `∑ Ji ∈ π.boxes, vol Ji = vol J`. Note that in the usual case where
+  `E = F = ℝ`, this equality holds in `(ℝ →L[ℝ] ℝ)` which is isomorphic
+  to `ℝ`.
+
+  Given a tagged partition `π` of `I`, the integral sum of `f` over `π` w.r.t.
+  `vol` is the sum of `vol J (f (π.tag J))` over all boxes `J` of `π`,
+  where `π.tag J` is the tag (sample point) associated with box `J`.
+
+- `y : F`. The value of the integral.
+  `HasIntegral I l f vol y` is a asserting that `y` is the limit of Riemann-style
+  integral sums. Following the design of `HasSum`/`tsum` for infinite sums,
+  `HasIntegral` is a predicate; the companion `BoxIntegral.integral`
+  returns a vector satisfying the predicate,
+  or zero if the function is not integrable.
+
 -/
+
+#print BoxIntegral.IntegrationParams.Riemann
+-- def BoxIntegral.IntegrationParams.Riemann : BoxIntegral.IntegrationParams :=
+-- { bRiemann := true, bHenstock := true, bDistortion := false }
+
+
+def HasRiemannIntegral (f : ℝ → ℝ) (a b : ℝ) (hab : a < b) (y : ℝ) :=
+  BoxIntegral.HasIntegral (ι := Fin 1)
+    (I := BoxIntegral.Box.mk (fun _ => a) (fun _ => b) (fun _ => hab))
+    (l := BoxIntegral.IntegrationParams.Riemann)
+    (f := fun x => f (x 0))
+    (vol := BoxIntegral.BoxAdditiveMap.volume)
+    (y := y)
 
 end Ex3
