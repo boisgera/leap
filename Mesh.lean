397b694c87946076c631350002365cbf68bbcf02
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-07-11 17:17:39 +0200

    Snapshot

--- Mesh.lean
+++ Mesh.lean
@@ -1,7 +1,7 @@
 import Lean
 import Batteries.Lean.Float
 
-namespace Mesh
+namespace STL
 
 structure Point where
   x : Float
@@ -23,6 +23,14 @@ deriving Repr
 
 #eval Float.toStringFull (0.1 + 0.2)
 -- "0.3000000000000000444089209850062616169452667236328125"
+
+def Vector.origin := Vector.mk 0 0 0
+
+instance : Inhabited Vector where
+  default := Vector.origin
+
+def Vector.toSTL (u : Vector) : String :=
+  s!"{u.1.toStringFull} {u.2.toStringFull} {u.3.toStringFull}"
 
 def Vector.add (u v : Vector) : Vector :=
   {
@@ -70,6 +78,14 @@ notation:max "‖" x "‖" => Vector.norm x
 #eval ‖Vector.mk 1.0 1.0 1.0‖
 -- 1.732051
 
+def Point.origin : Point := Point.mk 0 0 0
+
+instance : Inhabited Point where
+  default := Point.origin
+
+def Point.toSTL (p : Point) : String :=
+  s!"{p.1.toStringFull} {p.2.toStringFull} {p.3.toStringFull}"
+
 def Point.sub (q p : Point) : Vector :=
   Vector.mk (q.x - p.x) (q.y - p.y) (q.z - p.z)
 
@@ -79,8 +95,17 @@ instance : HSub Point Point Vector where
 #eval (Point.mk 1.0 2.0 3.0) - (Point.mk (-1.0) (-1.0) (-1.0))
 -- { x := 2.000000, y := 3.000000, z := 4.000000 }
 
+def Point.add (p : Point) (u : Vector) : Point :=
+  Point.mk (p.x + u.x) (p.y + u.y) (p.z + u.z)
+
+instance : HAdd Point Vector Point where
+  hAdd := Point.add
+
+#eval (Point.mk 1 1 1) + (Vector.mk 1 2 3)
+-- { x := 2.000000, y := 3.000000, z := 4.000000 }
+
 structure Facet where
-  vertex_1 : Point -- can I have f.1 notation? Or even f[1]?
+  vertex_1 : Point -- The f.1 notation is for free
   vertex_2 : Point
   vertex_3 : Point
 deriving Repr
@@ -104,11 +129,295 @@ def facet := Facet.mk (Point.mk 0 0 0) (Point.mk 1.0 0.0 0.0) (Point.mk 0.0 1.0 
 #eval facet.normal
 -- { x := 0.000000, y := 0.000000, z := 1.000000 }
 
-end Mesh
+def Facet.toSTL (f : Facet) : String :=
+s!"facet normal {f.normal.toSTL}
+    outer loop
+        vertex {f.1.toSTL}
+        vertex {f.2.toSTL}
+        vertex {f.3.toSTL}
+    endloop
+endfacet"
+
+-- not sure we need a new type here...
+structure Quad where
+  vertex_1 : Point
+  vertex_2 : Point
+  vertex_3 : Point
+  vertex_4 : Point
+
+def Quad.toList (q : Quad) : List Point :=
+  [q.vertex_1, q.vertex_2, q.vertex_3, q.vertex_4]
+
+def weightedSum (elts : List (Float × Point))
+    (origin : Point := default) : Point :=
+  -- form the vectors origin-to-points
+  let weightedVectors := elts.map (fun (w, p) => (w, p - origin))
+  -- combine the vectors
+  let weightedPoint := weightedVectors.foldl
+    (f := fun p (w, v) => p + w • v)
+    (init := (origin : Point))
+  weightedPoint
+
+#eval weightedSum [(0.5, (Point.mk 1 2 3)), (0.5, (Point.mk 3 2 1))]
+-- { x := 2.000000, y := 2.000000, z := 2.000000 }
+
+#eval weightedSum [
+  (0.25, Point.mk 0 0 0),
+  (0.25, Point.mk 1 0 0),
+  (0.25, Point.mk 1 1 0),
+  (0.25, Point.mk 0 1 0),
+]
+-- { x := 0.500000, y := 0.500000, z := 0.000000 }
+
+def Quad.split (q : Quad) : List Facet :=
+  let center := q.toList.map (fun p => (0.25, p)) |> weightedSum
+  [
+    Facet.mk q.1 q.2 center,
+    Facet.mk q.2 q.3 center,
+    Facet.mk q.3 q.4 center,
+    Facet.mk q.4 q.1 center,
+  ]
+
+
 
 structure Mesh where
-  facets : List Mesh.Facet
+  facets : List Facet
+
+def Mesh.toSTL (m : Mesh) (name : String := "") : String :=
+  let facetsSTL := m.facets |>.map (fun f => f.toSTL) |> String.intercalate "\n"
+  s!"solid {name}\n{facetsSTL}\nendsolid {name}"
+
 
 -- TODO: STL export
 
-def main := IO.println "mesh"
+def test_facet :=
+  Mesh.mk [Facet.mk (Point.mk 0 0 0) (Point.mk 1 0 0) (Point.mk 0 1 0)]
+  |>.toSTL
+  |> IO.println
+
+#eval test_facet
+-- solid
+-- facet normal 0 0 1
+--     outer loop
+--         vertex 0 0 0
+--         vertex 1 0 0
+--         vertex 0 1 0
+--     endloop
+-- endfacet
+-- endsolid
+
+def test_cube := do
+  let p_1 := Point.mk 0 0 0
+  let p_2 := Point.mk 1 0 0
+  let p_3 := Point.mk 1 1 0
+  let p_4 := Point.mk 0 1 0
+  let p_5 := Point.mk 0 0 1
+  let p_6 := Point.mk 1 0 1
+  let p_7 := Point.mk 1 1 1
+  let p_8 := Point.mk 0 1 1
+  let quad_1 := Quad.mk p_1 p_4 p_3 p_2
+  let quad_2 := Quad.mk p_1 p_2 p_6 p_5
+  let quad_3 := Quad.mk p_2 p_3 p_7 p_6
+  let quad_4 := Quad.mk p_3 p_4 p_8 p_7
+  let quad_5 := Quad.mk p_4 p_1 p_5 p_8
+  let quad_6 := Quad.mk p_5 p_6 p_7 p_8
+  let facets :=
+    quad_1.split ++
+    quad_2.split ++
+    quad_3.split ++
+    quad_4.split ++
+    quad_5.split ++
+    quad_6.split
+  let mesh := Mesh.mk facets
+  let meshSTL := mesh.toSTL "cube"
+  meshSTL |> IO.println
+  IO.FS.writeFile "cube.stl" meshSTL
+
+-- TODO:
+-- given a SDF and a quantized 3D range, get the active edges (with sign change)
+-- collect the corresponding quad and build the mesh.
+-- How do we deal with the fact that we don't want to have all (active and non
+-- active) edges in memory at the same time? Try a first version where we don't
+-- care? The iterator API is frightening ...
+
+def meshOfSdf
+  (f : Float → Float → Float → Float)
+  (min max : Point) (step : Float) : Mesh :=
+  sorry
+
+
+end STL
+
+
+def main := do
+  STL.test_cube
+-- solid cube
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 0 0 0
+--         vertex 0 1 0
+--         vertex 0.5 0.5 0
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 0 1 0
+--         vertex 1 1 0
+--         vertex 0.5 0.5 0
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 1 1 0
+--         vertex 1 0 0
+--         vertex 0.5 0.5 0
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 1 0 0
+--         vertex 0 0 0
+--         vertex 0.5 0.5 0
+--     endloop
+-- endfacet
+-- facet normal 1 0 0
+--     outer loop
+--         vertex 0 0 0
+--         vertex 0 1 0
+--         vertex 0 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal 1 0 0
+--     outer loop
+--         vertex 0 1 0
+--         vertex 0 1 1
+--         vertex 0 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal 1 0 0
+--     outer loop
+--         vertex 0 1 1
+--         vertex 0 0 1
+--         vertex 0 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal 1 0 0
+--     outer loop
+--         vertex 0 0 1
+--         vertex 0 0 0
+--         vertex 0 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 -1 0
+--     outer loop
+--         vertex 0 1 0
+--         vertex 1 1 0
+--         vertex 0.5 1 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 -1 0
+--     outer loop
+--         vertex 1 1 0
+--         vertex 1 1 1
+--         vertex 0.5 1 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 -1 0
+--     outer loop
+--         vertex 1 1 1
+--         vertex 0 1 1
+--         vertex 0.5 1 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 -1 0
+--     outer loop
+--         vertex 0 1 1
+--         vertex 0 1 0
+--         vertex 0.5 1 0.5
+--     endloop
+-- endfacet
+-- facet normal -1 0 0
+--     outer loop
+--         vertex 1 1 0
+--         vertex 1 0 0
+--         vertex 1 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal -1 0 0
+--     outer loop
+--         vertex 1 0 0
+--         vertex 1 0 1
+--         vertex 1 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal -1 0 0
+--     outer loop
+--         vertex 1 0 1
+--         vertex 1 1 1
+--         vertex 1 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal -1 0 0
+--     outer loop
+--         vertex 1 1 1
+--         vertex 1 1 0
+--         vertex 1 0.5 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 1 0
+--     outer loop
+--         vertex 1 0 0
+--         vertex 0 0 0
+--         vertex 0.5 0 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 1 0
+--     outer loop
+--         vertex 0 0 0
+--         vertex 0 0 1
+--         vertex 0.5 0 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 1 0
+--     outer loop
+--         vertex 0 0 1
+--         vertex 1 0 1
+--         vertex 0.5 0 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 1 0
+--     outer loop
+--         vertex 1 0 1
+--         vertex 1 0 0
+--         vertex 0.5 0 0.5
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 0 0 1
+--         vertex 0 1 1
+--         vertex 0.5 0.5 1
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 0 1 1
+--         vertex 1 1 1
+--         vertex 0.5 0.5 1
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 1 1 1
+--         vertex 1 0 1
+--         vertex 0.5 0.5 1
+--     endloop
+-- endfacet
+-- facet normal 0 0 -1
+--     outer loop
+--         vertex 1 0 1
+--         vertex 0 0 1
+--         vertex 0.5 0.5 1
+--     endloop
+-- endfacet
+-- endsolid cube
