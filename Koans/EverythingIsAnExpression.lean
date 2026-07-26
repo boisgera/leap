62d2394fe355e92fe5c911b488f4a09961eb1e88
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-07-26 11:50:09 +0200

    Snapshot

---
+++ Koans/EverythingIsAnExpression.lean
@@ -0,0 +1,47 @@
+
+/-!
+TODO:
+  - typed expression to be exact
+  - compare with Python
+  - simple, then inc complicated patterns taht are expression
+  - let are for convenience, change nothing
+  - concept of reduction (link to purity) of expressions
+  -> Computation is reduction of expression (mostly)
+
+-/
+
+
+/-!
+Even pseudo-imperative code (sequencing of actions/functions that may have
+side-effects) can be desugared into an expression. Here the key is the
+*monadic bind* operator `>>=`.
+-/
+
+/-! `App.lean`:-/
+def main : IO Unit := do
+  IO.print "Enter your name: "
+  let stdin ← IO.getStdin
+  let name ← stdin.getLine
+  IO.println s!"Hello, {name.trimAscii}"
+
+/-!
+Test with `lean --run App.lean
+-/
+
+/-!
+Equivalent lean code:
+-/
+
+def mainDesugared : IO Unit :=
+  let prompt : IO Unit :=
+    IO.print "Enter your name: "
+  let getStdin : IO IO.FS.Stream :=
+    IO.getStdin
+  let getName (stdin : IO.FS.Stream) : IO String :=
+    stdin.getLine
+  let greet (name : String): IO Unit :=
+    IO.println s!"Hello, {name.trimAscii}"
+  prompt
+    >>= fun (_ : Unit) => getStdin
+    >>= getName
+    >>= greet
