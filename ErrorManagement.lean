752066ded0efd94ef1e818a13c45f6167c3aae87
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-07-28 10:12:09 +0200

    Snapshot

--- ErrorManagement.lean
+++ ErrorManagement.lean
@@ -1,18 +1,38 @@
 /-!
+Title : 50 shades of grey
+
 Some functions may not "work properly" for some values.
-How do we deal with this?
+In pure FP, "errors" are out-of-domain arguments provided by the
+developer(/user). How do we deal with this? Many options
 
 - Undefined behavior: document, promise NOTHING if used outside the function
 domain (panic, program corruption, infinite loop, etc. all that is fair)
 
+- "junk" / default / fallback  value (sometimes user provided)
+
 - panic!
 
-- "junk" / default value
+- sentinel value (Python: -1 in str.find, None in dict.get, etc.)
 
-- sentinel value
-
-- Option
+- Option (/Except)
 
 - Props to avoid inputs outside the function domain (guarantee of no error).
+
+-/
+
+#print Except
+
+/-!
+Examples :
+
+  - Array access: []!, [].getD, []?, [] (or []'h)
+
+  - Float.log (-1.0)
+
+  - 1 - 2
+
+  - String.front
+
+  - String.take, String.
 
 -/
--- Koans/EverythingHasAType.lean
+++ Koans/EverythingHasAType.lean
@@ -2,4 +2,7 @@
 /-!
 "Values", Functions, Propositions, Types, etc.
 Even when it's implicit/inferred from the context.
+
+Compare with e.g. "Set theory" where ALMOST EVER QUESTION IS LEGIT,
+such as "is the intersection of ℝ and ℚ empty?", or "0 ∈ 1?"
 -/
